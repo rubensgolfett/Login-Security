2ed5b611ce6d083d2a1851fd9e3d20b4aa6b4597
rubensgolfett <lubisgolfetto@gmail.com>
2025-05-14 12:58:41 -0300

    modify the register css image

--- login-security/resources/views/register.php
+++ login-security/resources/views/register.php
@@ -10,38 +10,33 @@
 <body>
     <main>
         <section id="login">
-            <div id="image">
-            </div>
+            <div id="image"></div>
 
             <div id="forms">
                 <h1>Registrer</h1>
                 <p>Hello, are you new here? Sign up now below!</p>
 
                 <form action="<?= $_SERVER['PHP_SELF']?>" method="post" autocomplete="on">
+    
                     <div class="camp">
-                        
                         <label for="iusername"><span class="material-icons">person</span></label>
-
-                        <input type="text" name="username" id="iusername" placeholder="Create your UserName" require autocomplete="name" maxlength="100" class="btn">
-
-                        <label for="ilogin"><span class="material-icons">account_circle</span></label>
-
-                        <input type="email" name="create-email" id="ilogin" placeholder="User your E-mail" required autocomplete="email" maxlength="50" class="btn">
-                        
+                        <input type="text" name="username" id="iusername" placeholder="Create your UserName" required autocomplete="name" maxlength="100" class="btn">
                     </div>
 
                     <div class="camp">
+                        <label for="ilogin"><span class="material-icons">account_circle</span></label>
+                        <input type="email" name="create-email" id="ilogin" placeholder="Enter your E-mail" required autocomplete="email" maxlength="50" class="btn">
+                    </div>
 
+                    <div class="camp">
                         <label for="ipassword"><span class="material-icons">vpn_key</span></label>
                         <input type="password" name="create-password" id="ipassword" placeholder="Create your Password" required autocomplete="current-password" minlength="4" maxlength="20" class="btn">
                     </div>
 
                     <input type="submit" value="Register">
-                    <p>You have a account? <a href="login.php" id="min-bottom">Login here!</a></p>
+                    <p>You have an account? <a href="login.php" id="min-bottom">Login here!</a></p>
 
-                </a>
-                </form>
-            </div>
+                    </form>
         </section>
     </main>
 </body>
